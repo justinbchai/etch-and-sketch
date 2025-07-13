809431b39a69127eda2d2874384ad1d76c238b3f
Justin Chai <justinbchai@gmail.com>
2025-07-12 21:35:13 -0400

    added 'hover' effect so that the grid squares turn black on mouse over

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         square = document.createElement('div')
         square.classList.add('square')
+        square.addEventListener('mouseover', function (e) {
+            e.target.classList.add('colored');
+        });
         gridRow.appendChild(square)
     }
     gridContainer.appendChild(gridRow)
